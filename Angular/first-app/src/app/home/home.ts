3212ea50062b06693159af3e27008353abc44196
Qhaleed <reslykadiri@gmail.com>
2025-08-31 09:11:35 +0800

    interface/objects

--- Angular/first-app/src/app/home/home.ts
+++ Angular/first-app/src/app/home/home.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { HousingLocation } from '../housing-location/housing-location';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HousingLocation],
   template: `
    <section>
     <form>
@@ -10,9 +10,30 @@ import { Component } from '@angular/core';
       <button class="primary" type="button">Search</button>
     </form>
   </section>
+
+  <section class="results">
+    <app-housing-location></app-housing-location>
+  </section>
+
+
   `,
   styleUrl: './home.css'
 })
 export class Home {
+
+  // add read only image
+
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocation: HousingLocation = {
+    id: 999,
+    name: "tesssting home",
+    city: "Zamboanga City",
+    state: "Zamboanga del Sur",
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 1,
+    wifi: true,
+    laundry: true
+  }
 
 }
