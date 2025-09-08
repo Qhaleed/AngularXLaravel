1c10de37fb739846e84b1173e742926586aef317
Qhaleed <reslykadiri@gmail.com>
2025-09-08 20:44:08 +0800

    slugiffy

---
+++ Laravel/php_basics/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$myName = "Res";
+echo "The name of the coder is " . $myName;
+echo "\n";
+echo "Or you can do like this as well $myName";
+
+function doubleMe($userInput){
+
+    $x = $userInput * 2;
+    echo "$userInput x 2 = ";
+    return $x;
+
+}
+
+function tripleMe($userInput){return $userInput * 3;};
+
+echo doubleMe(10);
+
+echo "\n";
+echo tripleMe(30);
+
+
+
+?>
---
+++ Laravel/php_basics/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '089bcc1b66864663661729c459ddb7e17e10354f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '089bcc1b66864663661729c459ddb7e17e10354f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'cocur/slugify' => array(
+            'pretty_version' => 'v4.6.0',
+            'version' => '4.6.0.0',
+            'reference' => '1d674022e9cbefa80b4f51aa3e2375b6e3c14fdb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../cocur/slugify',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
