7b98b86777b4346c8456f5de269b534e9e1a2e57
Qhaleed <reslykadiri@gmail.com>
2025-09-12 23:34:14 +0800

    BoostBuddy

---
+++ Laravel/BoostBuddy/resources/views/Components/header.blade.php
@@ -0,0 +1,10 @@
+<div class="flex flex-col w-screen bg-black
+ text-white align-middle justify-center text-center gap-10 p-10">
+    <h1 class="text-5xl">
+        {{ $title }}
+    </h1>
+
+    <h3 class="justify-center text-center">
+        {{ $subtitle }}
+    </h3>
+</div>
---
+++ Laravel/BoostBuddy/resources/views/Components/navbar.blade.php
@@ -0,0 +1,7 @@
+<nav class="flex flex-row bg-gray-900 text-white w-full p-1">
+    <ul class="w-full h-10 justify-center text-center ">
+
+            {{ $slot }}
+
+    </ul>
+</nav>
