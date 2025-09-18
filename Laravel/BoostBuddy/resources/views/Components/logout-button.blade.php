6412cdc991d7c2539a45dfde24995ff9eff0e17f
Qhaleed <reslykadiri@gmail.com>
2025-09-18 13:50:53 +0800

    logout button

---
+++ Laravel/BoostBuddy/resources/views/Components/logout-button.blade.php
@@ -0,0 +1,8 @@
+
+<button
+{{-- class="px-4 py-2 rounded-md transition duration-200 font-medium border border-white text-white hover:bg-white hover:text-gray-900" --}}
+
+class="px-10 bg-blue-600 rounded-md hover:bg-blue-800 transition duration-200"
+id="logout-btn">
+    {{ $slot }}
+</button>
--- Laravel/BoostBuddy/resources/views/Components/navbar.blade.php
+++ Laravel/BoostBuddy/resources/views/Components/navbar.blade.php
@@ -1,16 +1,24 @@
 <nav class="flex items-center justify-between px-10 flex-row bg-gray-900 text-white w-full p-1 my-5">
-
+    {{-- Logo name  --}}
             {{ $slot }}
     <div>
 
 
         @if(Auth::check())
-      <h1>Welcome back {{Auth::user()->name}}</h1>
+        <div class="flex align-middle text-center justify-center gap-5">
+            <h1>Welcome back {{Auth::user()->name}}</h1>
+            <x-logout-button>
+                logout
+            </x-logout-button>
+        </div>
+
         @else
 
          <x-auth-button variant="primary" id="signup-btn" >
                 Sign Up
-            </x-auth-button> <x-auth-button variant="outline" id="login-btn">
+            </x-auth-button>
+
+            <x-auth-button variant="outline" id="login-btn">
                 Login
             </x-auth-button>
 @endif
